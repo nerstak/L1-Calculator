41ff37d3a06bb821db67ae77a9aa5d4081fae610
Karsten <roy.karst@gmail.com>
2017-12-20 22:04:09 +0100

    Work on division by zero

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+import re
 def type_calc(x):
     if x == "+" or x == "*" or x == "-" or x == "/":
         return 'operator'
@@ -27,25 +28,35 @@ def string_to_list_type(string): #Count number of parenthesis
             i+=1
     return list
 
-def calculation(poly_list):
+def calculation(poly_list): #Attention, ce paragraphe est à finir (check '((5+6)/(6*2-12))'). Conseil : utilise plusieurs variables de return, dont une validant que le calcul fonctionne
     calc = 0
-    print('pol',poly_list)
     for i in range(len(poly_list)):
         print('gg',poly_list[i])
         if i-1 > 0:
             if poly_list[i-1][0] == '*' and poly_list[i][1] == 'integer':
                 calc = calc * int(poly_list[i][0])
             elif poly_list[i-1][0] == '/' and poly_list[i][1] == 'integer':
-                calc = calc / int(poly_list[i][0])
+                try:
+                    calc = calc / int(poly_list[i][0])
+                except ZeroDivisionError:
+                    return "Division by zero not allowed"
         elif poly_list[i][1] == 'integer':
             calc = calc + int(poly_list[i][0])
     print("c",calc)
     return calc
             
-    
 
 def evaluate(polynom_list):
+    temp=[]
+    for i in range(len(polynom_list)):
+        temp.append(polynom_list[i][0])
+    print(temp)
+    temp = ''.join(temp)
+    if re.search(r"/\)*\(*0",temp) is not None:
+        return 'Division by zero not allowed'
     print('li',polynom_list)
+    if type(polynom_list) == type(''):
+        return polynom_list
     if polynom_list == None:
         return
     elif ('(','parenthesis') in polynom_list:
@@ -68,20 +79,9 @@ def evaluate(polynom_list):
             list2.append(polynom_list[i])
         for i in range(pos_p+1,len(polynom_list)):
             list3.append(polynom_list[i])
-        if list1 == [] and list3 == []:
-            print(13)
-            re = (string_to_list_type(str(evaluate(list2))))
-        elif list1 == []:
-            print(1)
-            re = (string_to_list_type(str(evaluate(list2)))) + list3
-        elif list3 == []:
-            print(3)
-            re = list1 + (string_to_list_type(str(evaluate(list2))))
-        else:
-            print(0)
-            re = list1 + (string_to_list_type(str(evaluate(list2)))) + list3
-        print('re',re)
-        return (evaluate(re))
+        ret = list1 + (string_to_list_type(str(evaluate(list2)))) + list3
+        print('ret',ret)
+        return (evaluate(ret))
 
     else:
         print('v')
@@ -92,7 +92,6 @@ def evaluate(polynom_list):
                 list1.append(polynom_list[i])
             for i in range(polynom_list.index(('+','operator'))+1,len(polynom_list)):
                 list2.append(polynom_list[i])
-            print('oy',int(evaluate(list1)) + int(evaluate(list2)))
             return evaluate(list1) + evaluate(list2)
         elif ('-','operator') in polynom_list:
             list1 = []
